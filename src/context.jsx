1d1a08f8c8f1083db2b416fe4eed4d92a289a11a
Víctor Peña Romero <vic.pero@icloud.com>
2022-06-18 09:47:59 -0500

    add context for state management

---
+++ src/context.jsx
@@ -0,0 +1,32 @@
+import { createContext, Component } from 'react'
+
+export const Context = createContext()
+
+class Provider extends Component {
+
+  state = {
+    darkTheme: false,
+  }
+
+  toggleTheme = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      darkTheme: !prevState.darkTheme,
+    }))
+  }
+
+  render() { 
+
+    const { state, toggleTheme } = this
+
+    return (
+      <Context.Provider value={{ state, toggleTheme }}>
+        { this.props.children }
+      </Context.Provider>
+    )
+
+  }
+
+}
+ 
+export default Provider
